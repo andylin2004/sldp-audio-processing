2fc53ea6a14e48f10b1e310f569785e60f4ef62b
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-11-03 08:56:27 -0400

    graph to show stuff done

--- audio_read.py
+++ audio_read.py
@@ -1,16 +1,22 @@
 import wave
 import numpy as np
+import matplotlib.pyplot as plt
+
 wav_obj = wave.open('StarWars60.wav', 'rb')
 
 sample_freq = wav_obj.getframerate()
 n_samples = wav_obj.getnframes()
-time = n_samples/sample_freq
-print(time)
+t_audio = n_samples/sample_freq
+print(t_audio)
 n_channels = wav_obj.getnchannels()
 print(n_channels)
 signal_wave = wav_obj.readframes(n_samples)
 signal_array = np.frombuffer(signal_wave, dtype=np.int16)
-l_channel = signal_array[0::2]
-r_channel = signal_array[1::2]
-print(l_channel)
-print(r_channel)
+times = np.linspace(0, n_samples/sample_freq, num=n_samples)
+plt.figure(figsize=(15,5))
+plt.plot(times, signal_array)
+plt.title("Left Channel")
+plt.ylabel("Signal Value")
+plt.xlabel("Time (s)")
+plt.xlim(0, t_audio)
+plt.show()
